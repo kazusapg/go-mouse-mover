1ee4c44fedf558fac2ca7ed24b3d4e053c589295
kazusa_pg <kazusaprog@gmail.com>
2022-08-09 20:46:07 +0900

    Add a test for stopping move mouse

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"testing"
 )
 
@@ -12,4 +13,13 @@ func TestInputIntervalMilliSecond(t *testing.T) {
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
+}
+
+func TestMoveMousePosition(t *testing.T) {
+	t.Run("Check to see if the context can be used to cancel successfully", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		mi := moveInfo{Positions: []position{{X: 0, Y: 0}}, IntervalMillisecond: 10000}
+		go moveMousePosition(ctx, mi)
+		cancel()
+	})
 }
